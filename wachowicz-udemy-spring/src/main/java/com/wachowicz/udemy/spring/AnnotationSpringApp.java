61053a009f88d5ac47f046662ce5fd91c0c1fd5f
Tomasz Wachowicz <wachowicztomek@gmail.com>
2018-09-08 20:59:29 +0200

    default bean Annotation

--- wachowicz-udemy-spring/src/main/java/com/wachowicz/udemy/spring/AnnotationSpringApp.java
+++ wachowicz-udemy-spring/src/main/java/com/wachowicz/udemy/spring/AnnotationSpringApp.java
@@ -10,7 +10,7 @@ public class AnnotationSpringApp {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         //get the bean from spring container
-        Coach coach = context.getBean("thatSillyCoach",Coach.class);
+        Coach coach = context.getBean("tennisCoach",Coach.class);
         //call a method on the bean
         System.out.println(coach.getDailyWorkout());
 
--- wachowicz-udemy-spring/src/main/java/com/wachowicz/udemy/spring/TennisCoach.java
+++ wachowicz-udemy-spring/src/main/java/com/wachowicz/udemy/spring/TennisCoach.java
@@ -2,7 +2,7 @@ package com.wachowicz.udemy.spring;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach{
 
     @Override
